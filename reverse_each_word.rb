66751b543c49e7dc809bacb6e7e0dfcd4b13acab
Bob Habig <rfhab2@gmail.com>
2018-02-07 05:15:54 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,14 @@
-def reverse_each_word(string)
-  string.split.map{|letters| letters.reverse}.join(" ")
+# def reverse_each_word(string)
+#   string.split.map{|letters| letters.reverse}.join(" ")
+# end
+
+
+def reverse_each_word(sentence1)
+  backward_sentence = []
+  sentence1.split.each do |word|
+    backward_sentence << word.reverse
 end
+backward_sentence.join(' ')
+end
+
+puts reverse_each_word("Hello there, and how are you?")
